765307e7a8e1c68e516f02a4fed2938379bf7b70
Stephen Hubbard <hubbard_stephen@yahoo.com>
2020-01-17 08:53:17 -0700

    -

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="/profile/9">
+                                    <a class="dropdown-item" href="/profile/1">
                                         {{ __('My Profile') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
